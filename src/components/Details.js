405ada54734f9c22b5b421e2fd365d7196dd6651
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-06-01 01:00:35 +0300

    gece yatmadan önceki son hali bug mevcut.

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,30 +3,38 @@ import { useParams } from "react-router-dom";
 
 function Details(props) {
   const params = useParams();
-  
- 
-    const foundAdvert = props.adverts.filter(item => item.id === params.advertId)
-    console.log(params.advertId)
-    
-    console.log(foundAdvert[0])
-    
 
- 
-  
+  const foundAdvert = props.adverts.filter(
+    (item) => item.id === params.advertId
+  );
+  console.log(params.advertId);
+
+  console.log(foundAdvert[0]);
+
   return (
-    <div className="mt-5">
-      <div className="card text-center">
-        <div className="card-header">Featured</div>
-        <div className="card-body">
-          <h5 className="card-title">{foundAdvert[0].name}</h5>
-          <p className="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
-          <img src={foundAdvert[0].img} alt="" style={{width: "25rem"}} className="card-img"/>
-          
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <div className="mt-5">
+            <div className="card text-center">
+              <div className="card-header">Featured</div>
+              <div className="card-body">
+                <h5 className="card-title">{foundAdvert[0].name}</h5>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <img
+                  src={foundAdvert[0].img}
+                  alt=""
+                  style={{ width: "25rem" }}
+                  className="card-img"
+                />
+              </div>
+              <div className="card-footer text-muted">TEXT</div>
+            </div>
+          </div>
         </div>
-        <div className="card-footer text-muted">TEXT</div>
       </div>
     </div>
   );
